7ba63fbda7d3d895fd5f1413d01bd4a544c86ef7
Yosshy <Yosshy_123@proton.me>
2025-11-30 07:26:41 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
-	new Typed('#Yosshy', {
-		strings: [
-			"Yosshy's HomePage",
-			"I'm Yosshy.<br>I am a Programmer."
-		],
-		typeSpeed: 100,
-		backSpeed: 50,
-		startDelay: 300,
-		backDelay: 800,
-		smartBackspace: false,
-		showCursor: true,
-		contentType: 'html',
-		loop: false
-	});
+  new Typed('#Yosshy', {
+    strings: ["Yosshy's HomePage"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    showCursor: true,
+    contentType: 'html',
+    loop: false,
+    onComplete: function(self) {
+      self.reset();
+      setTimeout(function() {
+        new Typed('#about', {
+          strings: ["I'm Yosshy.<br>I am a Programmer."],
+          typeSpeed: 100,
+          backSpeed: 0,
+          showCursor: true,
+          contentType: 'html',
+          loop: false
+        });
+      }, 500);
+    }
+  });
 });
 
 
